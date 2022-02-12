0ae6a448ce74a1cfc2e7c26b3ef154bae2e5bb5d
sebastian <sabestech@gmail.com>
2022-02-11 21:52:54 -0400

    funcionalidad de multiplicador implementada

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,16 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
+
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const third = document.querySelector("#tercer-numero");
+const fourth = document.querySelector("#cuarto-numero");
 const form = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multilpicar-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado-div2");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +19,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const thirdnumber = Number.parseInt(third.value);
+  const fourthnumber = Number.parseInt(fourth.value);
+
+  div2.innerHTML = "<p>" + multiplicar(thirdnumber, fourthnumber) + "</p>";
 });
